f11c205012c013969e2123d455bc0bc939d00abf
Harshal <harshalpatel2193@gmail.com>
2025-03-25 14:20:54 +0530

    Added auth pages,routing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return <div>{children}</div>;
 };
 
-export default RootLayout
+export default Layout;
